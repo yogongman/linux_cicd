c9a7658ca9ac18d8cd62807e25630488daf8a443
yogongman <yogongman@gmail.com>
2024-12-09 16:40:23 +0900

    hubtest

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-res.send("<h1> Incheon National University !!</h1>");
+res.send("<h1> Incheon National University !!!</h1>");
 });
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
